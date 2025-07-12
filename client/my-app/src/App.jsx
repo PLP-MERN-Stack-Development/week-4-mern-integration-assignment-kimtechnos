1a5ee497463e3c2474e10e607e2affb9fe569517
kimtechnos <franciskimani559@gmail.com>
2025-07-12 13:36:01 +0300

    Initialize frontend with Vite, Tailwind CSS, and project folder structure

---
+++ client/my-app/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+     <h2 className='text-2xl font-semibold text-red-600 p-4 bg-red-100 ronded-md shadow-md'>Hello</h2>
+    </div>
+  )
+}
+
+export default App
